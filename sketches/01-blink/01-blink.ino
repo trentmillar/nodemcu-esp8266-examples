0eec75dcfaba32bd4812451f182a251ea0e231bc
trent <trent.millar@gmail.com>
2020-02-15 11:23:37 -0700

    blink on esp8266 (no board diagram)

---
+++ sketches/01-blink/01-blink.ino
@@ -0,0 +1,10 @@
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(LED_BUILTIN, OUTPUT);
+  digitalWrite(LED_BUILTIN, LOW);
+}
+
+// the loop function runs over and over again forever
+void loop() { 
+}
+/Users/tmillar/dev/repo/aqua/nodemcu-esp8266-examples/sketches/02-connect-wifi/02-connect-wifi.ino
